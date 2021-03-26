1f8c73c4eb936e5c0343eb3c5b64a2f3abdf02b2
Rickey Shumway <eric.g.shumway@gmail.com>
2021-03-25 21:55:07 -0600

    more work with the players class

--- script.js
+++ script.js
@@ -3,17 +3,28 @@
 //     document.getElementById('h1-score').style.display = 'none';
 // })
 
-document.getElementById('row-p1').addEventListener('click', function(e) {
+// document.getElementById('row-p1').addEventListener('click', function(e) {
+let playerList = [];
+addPlayer = function (name, teeColor, hCap, position) {
+    let newPlayer = new Player(name, teeColor, hCap, position);
+    playerList[position] = newPlayer;
+
+    }
+document.getElementById('table-contain').addEventListener('click', function(e) {
+
     let findId = function(id) {
         let newArr = id.split('_');
         return newArr[1];
     }
+    let updateScore
     let splitId = findId(e.target.id);
+    console.log(splitId)
     let showInput = function(x) {
         document.getElementById('input_' + x).style.display = 'flex';
         document.getElementById('score_' + x).style.display = 'none';
-        console.log("this is gonna show " + x);
+       
     }
     showInput(splitId);
+
 })
 // console.log(rickey);
