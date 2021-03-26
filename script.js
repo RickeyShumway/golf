56ebc5b335c094ed8335733e993e7544b9086be2
Rickey Shumway <eric.g.shumway@gmail.com>
2021-03-26 10:18:08 -0600

    Player input

--- script.js
+++ script.js
@@ -12,19 +12,70 @@ addPlayer = function (name, teeColor, hCap, position) {
     }
 document.getElementById('table-contain').addEventListener('click', function(e) {
 
-    let findId = function(id) {
-        let newArr = id.split('_');
-        return newArr[1];
+    let findId = function(id, symbol) {
+        let newArr = id.split(symbol);
+        return newArr;
     }
-    let updateScore
-    let splitId = findId(e.target.id);
-    console.log(splitId)
+
+    let updateScore;
+    let splitId = findId(e.target.id, "_")[1];
+    let findSecondId = findId(splitId, "-")[0];
+    console.log("this is the first split: "+splitId, "this is the second split: " + findSecondId);
     let showInput = function(x) {
         document.getElementById('input_' + x).style.display = 'flex';
         document.getElementById('score_' + x).style.display = 'none';
-       
+        document.getElementById('input_' + x).focus();
     }
-    showInput(splitId);
+    let showNameInput = function(x) {
+        document.getElementById('name-input_' + x).style.display = 'flex';
+        
+        document.getElementById('name_' + x).style.display = 'none';
+        document.getElementById('name-input_' + x).focus();
+    }
+    if(e.target.className == 'p-name') {
+        showNameInput(findSecondId);
+    } else {
+        showInput(splitId);
+    }
+    
+    
 
-})
+}, true)
+document.getElementById('table-contain').addEventListener('blur', function(e) {
+    let findId = function(id, symbol) {
+        let newArr = id.split(symbol);
+        console.log(newArr)
+        return newArr;
+    }
+    let player;
+    let hole;
+    let inputValue;
+    let splitId = findId(e.target.id, '_')[1];
+    let getInputValue = function(x) {
+        let userInput = document.getElementById('input_' + x);
+
+        console.log("User input: " + userInput.value, 'Player and Hole: ' + x);
+        player = findId(x, "-")[0];
+        hole = findId(x,'-')[1];
+        inputValue = userInput.value;
+        userInput.style.display = 'none';
+    }
+    let getNameValue = function(x) {
+        let userInput = document.getElementById('name-input_' + x);
+        inputValue = userInput.value;
+        userInput.style.display = 'none';
+        document.getElementById('name_' + x).style.display = 'flex';
+    }
+
+    if(e.target.className == 'name-value') {
+        getNameValue(splitId);
+        console.log("this is second id")
+    } else {
+        getInputValue(splitId);
+    }
+    
+    console.log(player, hole, inputValue)
+
+
+}, true)
 // console.log(rickey);
